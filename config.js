643e330c16a1b8906a2f55988fd77ff3121ca81b
pupparn <natthawat.wk@gmail.com>
2025-02-24 16:17:43 +0700

    fix config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     secureKey: 'pricehub@Huahin',
-    priceHubDB: 'mysql://4Hhk89doDKEsuSc.root:3khLLbliipI7rMRD@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/pricehub_db'
+    priceHubDB: 'mysql://4Hhk89doDKEsuSc.root:3khLLbliipI7rMRD@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/pricehub_db?ssl={"rejectUnauthorized":true}'
 };
 
 export default config;
